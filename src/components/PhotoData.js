111ca3730dab5e3032d6cc3f89f1ff50e75d1c85
Stephanie Ann Solis <stephaniesolis@lambdastudents.com>
2021-05-30 21:37:30 -0600

    styling needs more work

--- src/components/PhotoData.js
+++ src/components/PhotoData.js
@@ -1,14 +1,27 @@
 import React from "react";
+import styled from 'styled-components';
 
 export default function PhotoData(props) {
     const {astroPhotoData} = props
 
+    const PhotoDataDiv = styled.div`
+        background: #191970;
+        color: #B0E0E6;
+        max-height: 300px;
+        // display: flex;
+        // flex-direction: column;
+        // align-items: center;
+        justify-content: space-between;
+        margin-bottom: 20px;
+    `;
+
     return (
-        <div className='photoData'>
+        <PhotoDataDiv>
             <h1>{astroPhotoData.title}</h1>
             <p>{astroPhotoData.date}</p>
+            <p>{astroPhotoData.copyright}</p>
             <p>{astroPhotoData.explanation}</p>
-        </div> 
+        </PhotoDataDiv> 
     )
         
 }
